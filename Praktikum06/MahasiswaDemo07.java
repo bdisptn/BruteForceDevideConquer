7884958324a1607684425d544c0983defde6b742
bdisptn <budisptn13@gmail.com>
2026-04-10 10:47:43 +0700

    Praktikum 2 jobsheet 6

--- Praktikum06/MahasiswaDemo07.java
+++ Praktikum06/MahasiswaDemo07.java
@@ -38,11 +38,13 @@ public class MahasiswaDemo07 {
         System.out.print("IPK: ");
         double cari = sc.nextDouble();
 
-        System.out.println("menggunakan sequential searching");
-        double posisi = list.sequentialSearching(cari);
-        int pss = (int)posisi;
-        list.tampilPosisi(cari, pss);
-        list.tampilDataSearch(cari, pss);
+        System.out.println("-------------------------------------------");
+        System.out.println("menggunakan binary search");
+        System.out.println("-------------------------------------------");
+        double posisi2 = list.findBinarySearch(cari, 0, jumMhs-1);
+        int pss2 = (int)posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
 
         sc.close();
     }
